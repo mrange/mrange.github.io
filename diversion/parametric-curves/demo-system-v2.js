fe441f11d2af1327225c3870342d273d6348639e
mrange <marten_range@hotmail.com>
2022-01-23 14:30:05 +0100

    Parametric curves

--- diversion/parametric-curves/demo-system-v2.js
+++ diversion/parametric-curves/demo-system-v2.js
@@ -30,26 +30,6 @@ function smoothstep(edge0, edge1, x) {
   return t * t * (3.0 - 2.0 * t);
 }
 
-function create_text_image(width, height, ...texts) {
-  const image = document.getElementById("offscreen_canvas");
-  image.width = width;
-  image.height= height;
-  const ctx   = image.getContext("2d");
-
-  ctx.beginPath();
-  ctx.rect(0, 0, ctx.canvas.width, ctx.canvas.height);
-  ctx.fillStyle = "white";
-  ctx.font      = "200px Faster One";
-  ctx.textAlign = "center";
-  for (const textKey in texts) {
-    const [w, h, text] = texts[textKey];
-    ctx.fillText(text, w*ctx.canvas.width, h*ctx.canvas.height);
-  }
-  ctx.restore();
-
-  return image;
-}
-
 function vector3(x, y, z) {
   return new Float32Array([
     x,
